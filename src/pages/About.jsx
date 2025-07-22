cf98e6a56b18055ac13be4e5b590977a7bcc1df1
Anshit Rangra <anshit22rangra@gmail.com>
2025-07-22 21:05:39 +0530

    fixing bug

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl font-bold blink-red text-red-500 mb-3 sm:mb-4">
             Choose the Red Pill
           </h1>
-          <NavLink to="/buy-book">
+          <NavLink to="/order-details">
             <button className="cursor-pointer px-5 sm:px-6 md:px-7 lg:px-8 py-2 sm:py-2.5 md:py-3  bg-red-600 hover:bg-red-700 transition duration-300 rounded-lg font-medium shadow-md hover:shadow-lg hover:shadow-red-900/30 text-white text-sm sm:text-base md:text-lg lg:text-lg xl:text-xl">
               Know the Answer
             </button>
